78afdb035f1e1707491d91948a3918ea7a7e0995
Bartosz Wilk <bartosz.wilk@grupawp.pl>
2017-06-04 19:54:07 +0200

    [data][network] add: persistent cookie storage based on shared prefs

--- app/src/main/java/pl/sggw/stosprzepelniony/di/DIProvider.java
+++ app/src/main/java/pl/sggw/stosprzepelniony/di/DIProvider.java
@@ -2,6 +2,9 @@ package pl.sggw.stosprzepelniony.di;
 
 import android.content.Context;
 
+import pl.sggw.stosprzepelniony.data.network.util.persistent_cookie_storage.PersistentCookieStorage;
+import pl.sggw.stosprzepelniony.data.network.util.persistent_cookie_storage.impl.shared_preferences.SharedPreferencesPersistentCookieStorage;
+
 public class DIProvider {
 
     private static DIProvider instance;
@@ -19,6 +22,11 @@ public class DIProvider {
 
     public void init(Context context) {
         this.appContext = context;
+    }
+
+    public PersistentCookieStorage getPersistentCookieStorage() {
+        assertNonNullContext();
+        return new SharedPreferencesPersistentCookieStorage(appContext);
     }
 
     private void assertNonNullContext() {
