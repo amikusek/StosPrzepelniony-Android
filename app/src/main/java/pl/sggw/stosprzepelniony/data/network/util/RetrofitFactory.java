687e444b8822b1b23a920db2f34be5bd5a4dee11
Bartosz Wilk <bartosz.wilk@grupawp.pl>
2017-06-04 19:55:03 +0200

    [data][network] add: RetrofitFactory with cookies interceptors

---
+++ app/src/main/java/pl/sggw/stosprzepelniony/data/network/util/RetrofitFactory.java
@@ -0,0 +1,30 @@
+package pl.sggw.stosprzepelniony.data.network.util;
+
+import pl.sggw.stosprzepelniony.data.network.util.interceptors.RestoreCookiesFromPersistentStorageInterceptor;
+import pl.sggw.stosprzepelniony.data.network.util.interceptors.SaveCookiesToPersistentStorageInterceptor;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitFactory {
+
+    private static final String BASE_URL = "https://api.stackexchange.com";
+    private Retrofit retrofit;
+
+    public RetrofitFactory() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    private OkHttpClient createCookiesPersistentHttpClient() {
+        return new OkHttpClient.Builder()
+                .addInterceptor(new RestoreCookiesFromPersistentStorageInterceptor())
+                .addInterceptor(new SaveCookiesToPersistentStorageInterceptor())
+                .build();
+    }
+}
