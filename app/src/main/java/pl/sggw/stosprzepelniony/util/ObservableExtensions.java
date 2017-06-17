5c81d158c83e1666fa657197f7d93d486a2022c8
Aleksandra Mikusek <xamikusek@gmail.com>
2017-06-17 22:07:38 +0200

    [util][rx] add: default stream retrying logic for Completables

--- app/src/main/java/pl/sggw/stosprzepelniony/util/ObservableExtensions.java
+++ app/src/main/java/pl/sggw/stosprzepelniony/util/ObservableExtensions.java
@@ -1,11 +1,19 @@
 package pl.sggw.stosprzepelniony.util;
 
+import io.reactivex.CompletableTransformer;
 import io.reactivex.ObservableTransformer;
 import io.reactivex.functions.Consumer;
 
 public class ObservableExtensions {
 
-    public static <T> ObservableTransformer<T, T> withRetryErrorLogic(Consumer<Throwable> onErrorAction) {
+    public static <T> ObservableTransformer<T, T> withObservableRetryErrorLogic(Consumer<Throwable> onErrorAction) {
+        return stream ->
+                stream
+                        .doOnError(onErrorAction)
+                        .retry();
+    }
+
+    public static CompletableTransformer withCompletableRetryErrorLogic(Consumer<Throwable> onErrorAction) {
         return stream ->
                 stream
                         .doOnError(onErrorAction)
