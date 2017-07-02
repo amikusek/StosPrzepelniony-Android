b11fb94a97ba2a8616545f4de52acc883c21af15
Aleksandra Mikusek <xamikusek@gmail.com>
2017-07-02 23:04:13 +0200

    [categories] improv: getting real categories with fake childrens

--- app/src/main/java/pl/sggw/stosprzepelniony/viper/categories/CategoriesInteractor.java
+++ app/src/main/java/pl/sggw/stosprzepelniony/viper/categories/CategoriesInteractor.java
@@ -2,34 +2,39 @@ package pl.sggw.stosprzepelniony.viper.categories;
 
 import com.mateuszkoslacz.moviper.base.interactor.BaseRxInteractor;
 
-import pl.sggw.stosprzepelniony.data.entity.Category;
-import pl.sggw.stosprzepelniony.viper.categories.adapter.item.RootCategory;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+import pl.sggw.stosprzepelniony.data.entity.Category;
+import pl.sggw.stosprzepelniony.data.network.util.RetrofitFactory;
+import pl.sggw.stosprzepelniony.viper.categories.adapter.item.RootCategory;
 
 class CategoriesInteractor
         extends BaseRxInteractor
         implements CategoriesContract.Interactor {
 
+    private RetrofitFactory retrofitFactory = new RetrofitFactory();
+
     @Override
     public Observable<List<RootCategory>> getCategories() {
-//        return Observable.just(
-//
-//        )
-        List<RootCategory> categories = new ArrayList<>();
+
         List<Category> children = new ArrayList<>();
         children.add(new Category(134, "Children 1"));
         children.add(new Category(243, "Children 2"));
         children.add(new Category(3434, "Children 3"));
         children.add(new Category(4342, "Children 4"));
-        categories.add(new RootCategory("Category one", children));
-        categories.add(new RootCategory("Category two", children));
-        categories.add(new RootCategory("Category three", children));
 
-        return Observable.just(categories);
+        return retrofitFactory
+                .getCategoriesAPI()
+                .getCategories()
+                .flatMap(categories ->
+                        Observable
+                                .fromIterable(categories)
+                                .map(category -> new RootCategory(category.getCategoryName(), children))
+                                .toList().toObservable())
+                .subscribeOn(Schedulers.io());
     }
 
 }
